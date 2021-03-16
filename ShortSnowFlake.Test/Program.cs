7d56bc791177c0d07c66744a1df17099dd9b9b6c
Yang, Edward <yrz1994@gmail.com>
2021-03-16 21:30:43 +0800

    Add Test

---
+++ ShortSnowFlake.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShortSnowFlake.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var idWoker = new ShortIdWorker(0);
+            var id = idWoker.NextId();
+            var translateId = idWoker.TranslateTimespan((long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds);
+            Console.WriteLine(id);
+            Console.WriteLine(translateId);
+            Console.ReadKey();
+        }
+    }
+}
